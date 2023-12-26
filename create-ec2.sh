8509a429964a6f6e2bf7df8ea8ea7c76e59c505e
surendra Babu C <surendra9703802865@gmail.com>
2023-12-26 18:47:56 +0530

    First Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,3 +1,4 @@
 for component in frontend mongodb catalogue ; do
   echo ${component}
+  aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t3.micro --key-name MyKeyPair
 done
